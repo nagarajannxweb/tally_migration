d335dc0b7df15a755d697f89d035c44fb1e3f2ca
balanxweb <bala@nxweb.in>
2026-02-11 13:21:51 +0000

    create fixtures for account custom field

--- tally_migration/hooks.py
+++ tally_migration/hooks.py
@@ -242,3 +242,7 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
+fixtures = [{
+        "doctype":"Custom Field", "filters":{"module":["in",["Tally Migration"]]}
+    }]
